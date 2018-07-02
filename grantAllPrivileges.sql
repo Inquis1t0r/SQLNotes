10f654d5374d5a823c1b59a3bb1e2af35487d83a
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-07-02 08:42:26 +0200

    Update grantAllPrivileges.sql

--- grantAllPrivileges.sql
+++ grantAllPrivileges.sql
@@ -1,1 +1,11 @@
 
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+SHOW GRANTS FOR root;
+grant all privileges on *.* to 'root'@'localhost' identified by 'root';
+
+#might be useful
+SHOW STATUS;
+SHOW VARIABLES;
+SHOW DATABASES;
+
+SHOW COLLATION;
