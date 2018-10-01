9023544adb3a8c0cccc2736f779191c1b122ab4a
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-10-01 10:21:43 +0200

    correct select for counter set

--- findDuplicateKeys.sql
+++ findDuplicateKeys.sql
@@ -1,2 +1,2 @@
-SELECT recno, COUNT(*) counter FROM article GROUP BY recno HAVING counter > 1;
-SELECT recno, COUNT(*) counter FROM artfou GROUP BY recno HAVING counter > 1;
+SELECT recno, COUNT(recno) counter FROM article GROUP BY recno HAVING counter > 1;
+SELECT recno, COUNT(recno) counter FROM artfou GROUP BY recno HAVING counter > 1;
