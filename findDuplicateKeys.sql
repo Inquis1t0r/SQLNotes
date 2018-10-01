5b64018b0516d65b05fbf4c55985da3731cb2acb
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-10-01 10:23:09 +0200

    select update

--- findDuplicateKeys.sql
+++ findDuplicateKeys.sql
@@ -1,2 +1,2 @@
-SELECT recno, COUNT(recno) counter FROM article GROUP BY recno HAVING counter > 1;
-SELECT recno, COUNT(recno) counter FROM artfou GROUP BY recno HAVING counter > 1;
+SELECT code, COUNT(recno) counter FROM article GROUP BY recno HAVING counter > 1;
+SELECT article, COUNT(recno) counter FROM artfou GROUP BY recno HAVING counter > 1;
